ad3cffa651b1c31a5f9ef20e19bf8b0b99823a17
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2014-10-05 01:03:05 -0400

    fixed split function and stack overflow, but still some issues

--- algorithms/emma-and-sum-of-products.py
+++ algorithms/emma-and-sum-of-products.py
@@ -1,3 +1,7 @@
+# Initially written for memoization, I found that the recursion stack was overflowing
+# So I added the loop which computes everything first
+# The program would have been more efficient had I written it using traditional dynamic programming
+
 import sys
 
 cache = {}
@@ -19,9 +23,18 @@ def calcSum (index, k):
     return cache[key]
 
 
+N = int(sys.stdin.readline())
+numbers = [int(x) for x in sys.stdin.readline().split()]
 
-sys.stdin.readline()
-numbers = [int(x) for x in sys.stdin.readline().split(' ')]
+
+# Compute everything first so as not to overflow the recursion stack
+i = N
+while i >= 0:
+    for j in range(1,N+1):
+        calcSum(i,j)
+    i = i - 1
+
+
 
 tests = int(sys.stdin.readline())
 for i in range(tests):
