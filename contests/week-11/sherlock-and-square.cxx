07cd19c9f53239d1f5e36c0f22b66bd8f0c771ba
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2014-10-07 21:37:42 -0400

    redid day 1 challenge and solved day 2

--- contests/week-11/sherlock-and-square.cxx
+++ contests/week-11/sherlock-and-square.cxx
@@ -1,39 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <map>
 
 int main ()
 {
-	const long mod_value = 1000000007;
+	const ulong mod_value = 1000000007;
+
 	uint T;
 	std::cin >> T;
-
-	std::map<long,int> position_map;
-	std::vector<long> solutions (T);
-
-	for (uint t = 0; t != T; ++t)
+	while (T--)
 	{
-		long n;
-		std::cin >> n;
-		position_map[n] = t;
-	}
+		ulong N;
+		std::cin >> N;
 
-	long total = 4;
-	int t = 0;
-	long increment = 2;
-	for (std::pair<long,int> const & pair : position_map)
-	{
-		while (t != pair.first)
+		ulong ans = 2;
+		for (ulong i = 0; i != N; ++i)
 		{
-			total += increment;
-			total %= mod_value;
-			increment <<= 1;
-			increment %= mod_value;
-			++t;
+			ans %= mod_value;
+			ans <<= 1;
 		}
-		solutions[pair.second] = total;
-	}
+		ans += 2;
+		ans %= mod_value;
 
-	for (long l : solutions)
-		std::cout << l << '\n';
+		std::cout << ans << '\n';
+	}
 }
