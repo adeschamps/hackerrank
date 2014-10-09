b0f26654ae1e7dd2f404cee3b6c0fddb06895d5b
Anthony Deschamps <anthony.j.deschamps@gmail.com>
2014-10-09 15:17:47 -0400

    will go down suboptimal paths

--- contests/week-11/training-the-army.cxx
+++ contests/week-11/training-the-army.cxx
@@ -1,6 +1,6 @@
 #include <algorithm>
 #include <iostream>
-// #include <iterator>
+#include <list>
 #include <set>
 #include <vector>
 
@@ -9,7 +9,35 @@ struct wizard
 	std::set<int> from;
 	std::set<int> to;
 
+	void cast (std::vector<int> & skills, int f, int t)
+	{
+#if DEBUG
+		std::cout << "Cast from " << f << " to " << t << '\n';
+#endif
+		--skills[f];
+		from.erase(f);
+		++skills[t];
+		to.erase(t);
+	}
 };
+
+// Remove wizards who have run out of transformations
+void retire (std::list<wizard> & wizards)
+{ wizards.remove_if([] (wizard const & w) { return w.from.empty() || w.to.empty(); }); }
+
+// struct skill
+// {
+// 	int have = 0;
+// 	int want = 0;
+// 	// std::list<int> requests;
+// 	std::queue<int> requests;
+// };
+
+// struct request
+// {
+// 	int from;
+// 	int to;
+// }
 
 std::istream & operator >> (std::istream & is, wizard & w)
 {
@@ -20,14 +48,14 @@ std::istream & operator >> (std::istream & is, wizard & w)
 	for (int i = 0; i != count; ++i)
 	{
 		is >> skill;
-		w.from.insert(skill);
+		w.from.insert(skill-1);
 	}
 
 	is >> count;
 	for (int i = 0; i != count; ++i)
 	{
 		is >> skill;
-		w.to.insert(skill);
+		w.to.insert(skill-1);
 	}
 
 	return is;
@@ -42,9 +70,13 @@ int main ()
 	for (int n = 0; n != N; ++n)
 		std::cin >> skillsets[n];
 
-	std::vector<wizard> wizards (T);
+	std::list<wizard> wizards;
 	for (int t = 0; t != T; ++t)
-		std::cin >> wizards[t];
+	{
+		wizard w;
+		std::cin >> w;
+		wizards.push_back(std::move(w));
+	}
 
 #if DEBUG
 	std::cout << "Skills:\n";
@@ -52,7 +84,51 @@ int main ()
 		std::cout << s << '\t';
 	std::cout << '\n';
 
-	std::cout << "Wizards:\n";
+	std::cout << "Wizards: " << wizards.size() << "\n";
+	for (wizard const & w : wizards)
+	{
+		for (int s : w.from)
+			std::cout << s << '\t';
+		std::cout << "==>\t";
+		for (int s : w.to)
+			std::cout << s << '\t';
+		std::cout << '\n';
+	}
+	std::cout << '\n';
+#endif
+
+	// int max_depth = 1;
+	for (int to = 0; to != N; ++to)
+		if (skillsets[to] == 0)
+		{
+#if DEBUG
+			std::cout << "want skill in " << to << '\n';
+#endif
+			for (wizard & w : wizards)
+				if (w.to.count(to))
+				{
+#if DEBUG
+					std::cout << "Maybe a wizard can help\n";
+#endif
+					for (int from = 0; from != N; ++from)
+						if (skillsets[from] > 0 && w.from.count(from))
+						{
+							w.cast (skillsets, from, to);
+						}
+				}
+			retire (wizards);
+		}
+
+
+
+
+#if DEBUG
+	std::cout << "Skills:\n";
+	for (int s : skillsets)
+		std::cout << s << '\t';
+	std::cout << '\n';
+
+	std::cout << "Wizards: " << wizards.size() << "\n";
 	for (wizard const & w : wizards)
 	{
 		for (int s : w.from)
@@ -63,4 +139,5 @@ int main ()
 		std::cout << '\n';
 	}
 #endif
+
 }
